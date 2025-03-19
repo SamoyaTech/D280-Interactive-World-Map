a1ef4bc2b6998b3523ce78e99a55c9cffa5fb1c8
Samoya <sbec219@wgu.edu>
2025-03-19 12:33:48 -0500

    configured routing

--- d280-app/src/app/app.routes.ts
+++ d280-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { WorldComponent } from './world/world.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "map", component : WorldComponent},
+    { path: '' , redirectTo:'/map', pathMatch:'full' }
+];
+
+
+
+
