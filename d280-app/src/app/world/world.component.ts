dc7cf4cfd105f08fff058aa9d36ff1e6d90f8089
Samoya <sbec219@wgu.edu>
2025-03-22 14:25:25 -0500

    event binding-part f

--- d280-app/src/app/world/world.component.ts
+++ d280-app/src/app/world/world.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input , EventEmitter, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,11 +9,48 @@ import { CommonModule } from '@angular/common';
 })
 export class WorldComponent {
   @Input() countryDetails:any;
-  
 
-  //mouseover
-  
-    
+  @Output() regionSelected = new EventEmitter<string>(); // Emit selected country code
+
+
+  onMouseOver(event:MouseEvent):void {
+    const target = event.target as SVGElement; //get elemtnthat triggered the event
+
+    if (target.tagName === 'path') {
+      const regionName = target.getAttribute('name');
+      if (regionName) {
+        console.log(`Hovered over: ${regionName}`);
+        // save original color
+        if(!target.getAttribute('data-orginal-color')){
+          const currentColor = target.style.fill || 'black'; //default to black if no color is set
+          target.setAttribute('data-orginal-color', currentColor);//save orginal color
+        }
+        target.style.fill = 'yellow'; //highlight region
+      }
+    }
   }
 
+  onPathClick(event:MouseEvent): void {
+    const target = event.target as SVGElement;
 
+    if (target.tagName === 'path'){
+      const countryCode = target.getAttribute('id');//get countrycode(2-letter ISO code)
+      if (countryCode) {
+        console.log(`Clicked on: ${countryCode}`);
+        this.regionSelected.emit(countryCode); //call API to fetch country details
+      }
+    }
+  }
+
+  onMouseOut(event: MouseEvent): void{
+    const target = event.target as SVGAElement;
+
+    if (target.tagName === 'path') {
+      const originalColor = target.getAttribute('data-orginal-color');
+      if (originalColor) {
+        target.style.fill = originalColor;
+      }
+    }
+  }
+
+}
